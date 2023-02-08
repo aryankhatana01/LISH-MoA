c421fc18da2e6c636393caa6add6d652b60a89f9
aryankhatana01 <aryankhatana01@gmail.com>
2023-02-08 19:00:49 +0530

    added some docstrings

--- src/dataset.py
+++ src/dataset.py
@@ -12,9 +12,15 @@ class MoADataset(Dataset):
         self.targets = targets
 
     def __len__(self):
+        '''
+        Returns the length of the dataset.
+        '''
         return len(self.feats)
 
     def __getitem__(self, idx):
+        '''
+        Returns the idx-th item of the dataset as Xs and Ys.
+        '''
         return {
             'x': torch.tensor(self.feats[idx, :], dtype=torch.float),
             'y': torch.tensor(self.targets[idx, :], dtype=torch.float)
