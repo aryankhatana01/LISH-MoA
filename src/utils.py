34c45ca2a68cd79afb2b03a5eecb2d51c74c9463
aryankhatana01 <aryankhatana01@gmail.com>
2023-02-08 21:18:17 +0530

    reformatted

--- src/utils.py
+++ src/utils.py
@@ -15,7 +15,7 @@ def seed_everything(seed=42):
     This function seeds everything and makes the results reproducible/deterministic.
     Input:
         seed: int, seed to be used
-    
+
     Returns:
         None
     '''
@@ -26,6 +26,7 @@ def seed_everything(seed=42):
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
 
+
 def PCA_g(data, n_components=50):
     '''
     This function performs PCA on the given data for the genes.
@@ -33,16 +34,17 @@ def PCA_g(data, n_components=50):
     Input:
         data: pandas dataframe, data on which PCA is to be performed
         n_components: int, number of components to be used for PCA
-    
+
     Returns:
         transformed_data: pandas dataframe, transformed data
     '''
-    GENES = [col for col in data.columns if col.startswith('g-')] # All the genes columns
-    data_ = pd.DataFrame(data[GENES]) # Selecting the data of only the genes columns
-    data_ = PCA(n_components=n_components, random_state=42).fit_transform(data_[GENES]) # Performing PCA
+    GENES = [col for col in data.columns if col.startswith('g-')]  # All the genes columns
+    data_ = pd.DataFrame(data[GENES])  # Selecting the data of only the genes columns
+    data_ = PCA(n_components=n_components, random_state=42).fit_transform(data_[GENES])  # Performing PCA
     feats = pd.DataFrame(data_, columns=[f'pca_G-{i}' for i in range(n_components)])
     final_features = pd.concat((data, feats), axis=1)
     return final_features
+
 
 def PCA_c(data, n_components=50):
     '''
@@ -51,13 +53,13 @@ def PCA_c(data, n_components=50):
     Input:
         data: pandas dataframe, data on which PCA is to be performed
         n_components: int, number of components to be used for PCA
-    
+
     Returns:
         transformed_data: pandas dataframe, transformed data
     '''
-    CELLS = [col for col in data.columns if col.startswith('c-')] # All the genes columns
-    data_ = pd.DataFrame(data[CELLS]) # Selecting the data of only the genes columns and creating a dataframe
-    data_ = PCA(n_components=n_components, random_state=42).fit_transform(data_[CELLS]) # Performing PCA
+    CELLS = [col for col in data.columns if col.startswith('c-')]  # All the genes columns
+    data_ = pd.DataFrame(data[CELLS])  # Selecting the data of only the genes columns and creating a dataframe
+    data_ = PCA(n_components=n_components, random_state=42).fit_transform(data_[CELLS])  # Performing PCA
     feats = pd.DataFrame(data_, columns=[f'pca_C-{i}' for i in range(n_components)])
     final_features = pd.concat((data, feats), axis=1)
     return final_features
