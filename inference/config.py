2b71c0be766b612875959821fdf449d02023c246
aryankhatana01 <aryankhatana01@gmail.com>
2023-02-09 14:15:58 +0530

    config file for the inference module

---
+++ inference/config.py
@@ -0,0 +1,12 @@
+'''
+This is the config file for the inference module.
+It wouldn't contain any training related parameters.
+'''
+
+import torch
+
+
+class CFG:
+    batch_size = 32
+    model_dir = '../models/'
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
